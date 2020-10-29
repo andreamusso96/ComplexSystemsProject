4273c42a9d98930eb3cf066587725c06fc118eaf
flavio-dalessi <73491257+flavio-dalessi@users.noreply.github.com>
2020-10-29 17:19:02 +0100

    Update agent.py
    I hope there's not too many mistakes...
    I don't know how are we're providing the information about which providers are sharing the news and which not, because  we need to know that for calculating  the truth likelihood. Is the way I'm using the news is correct (how fitness and time being around are accessible, or how these are going to be?). One more thing that I'm not sure about is if it's correct that the truth likelihood has to be changed in the agent (when calling the function that calculates it) or if just the value has to be returned.

--- code/classes/agent.py
+++ code/classes/agent.py
@@ -1,6 +1,6 @@
 """ Agent class """
 import numpy as np
-
+import math
 
 class Agent:
     """
@@ -46,17 +46,28 @@ class Agent:
         assert 0 <= truth_weight <= 1, 'Invalid value for truth_weight. Value should be between 0 and 1'
         assert np.isclose(1, np.sum(trust_in_providers)), 'Invalid value for trust_in_providers. Array should be normalized'
 
-        # TODO: Implement constructor (Do not forget to initialize has_shared attribute)
+        # Constructor 
+        self.share_threshold = share_threshold
+        self.truth_likelihood = truth_likelihood
+        self.truth_weight = truth_weight
+        self.trust_in_providers = trust_in_providers
+        self.has_shared = False
         pass
 
     def compute_truth_likelihood(self):
         """
         Computes the truth likelihood of the news based on the trust in the information providers
-
+ 
         :return: float
             Truth likelihood of the news
         """
-        # TODO: Implement calculation of truth likelihood of news
+        
+        # one should before set to 0 the components in trust_in_providers which providers are not sharing the news, how ?
+        self.truth_likelihood = np.sum(self.trust_in_providers)+ (np.random([1])-0.5)/100
+        # adding 1% random stochastic noise
+        return self.truth_likelihood
+        
+        
         pass
 
     def compute_excitement(self, news):
@@ -68,7 +79,10 @@ class Agent:
         :return: float
             Excitement score of the news
         """
-        # TODO: Implement calculation of excitement score
+        
+        return (self.truth_weight * self.compute_truth_likelihood(self) + (1-self.truth_weight) * news.fitness) * math.exp(- news.time)
+        # assuming news has attributes time (how much times has been around) and fitness
+        
         pass
 
     def is_sharing(self, news):
@@ -84,7 +98,15 @@ class Agent:
         :return: bool
             If the agent shares the news
         """
-        # TODO: Implement sharing logic
+        # Sharing logic
+        if self.has_shared == True: 
+            return False
+        if self.compute_excitement(self, news) >= self.share_threshold:
+            self.has_shared = True
+            return True 
+        else:
+            return False
+        
         pass
 
     def __str__(self):
